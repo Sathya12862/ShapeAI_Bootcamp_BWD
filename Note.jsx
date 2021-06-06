ab1cdf7d633bcb4a4556de168eff35351bdc339e
Sathya12862 <85440074+Sathya12862@users.noreply.github.com>
2021-06-06 19:41:45 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>JavaScript and React.js</h1>
+      <p>
+        This course is conducted by ShapeAI Bootcamp. This course is taken by
+        Shaurya Sinha sir. This is an amazing Bootcamp. Certificates will be
+        provided for the students who has hundred percentage attendance as well
+        as correct project submission.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
